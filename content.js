899ce8cec70a088ee5397921cb5a8f6b21c34526
Tanny <ryosuke.tanimura+dev@gmail.com>
2024-09-15 23:13:26 +0900

    add: iconを追加

--- content.js
+++ content.js
@@ -15,20 +15,24 @@ function getISBN() {
 // ボタンを追加する関数
 function addCalilButton(isbn) {
   let button = document.createElement("button");
-  button.textContent = "Calilで確認";
+  button.textContent = "図書館でさがす";
 
   // ボタンのデザインを指定
-  button.style.width = "100%"; // 左右に目一杯広げる
-  button.style.height = "29px"; // 左右に目一杯広げる
-  button.style.padding = "1px 6px"; // 上下1px, 左右6pxのパディング
-  button.style.textAlign = "center"; // 文字を中央に配置
-  button.style.color = "white"; // 文字色を白に
-  button.style.border = "none"; // ボーダーを消す
-  button.style.backgroundColor = "#2ab6e9"; // 背景色を#2ab6e9に
-  button.style.cursor = "pointer"; // マウスオーバー時のカーソルをポインタに
-  button.style.fontSize = "14px"; // フォントサイズを指定
-  button.style.marginBottom = "8px"; // 下に8pxのマージンを追加
-  button.style.borderRadius = "100px"; // 角を丸める（5pxの半径）
+  button.style.width = "100%";       // 幅を左右に目一杯広げる
+  button.style.height = "29px";      // 高さを29pxに設定
+  button.style.padding = "1px 6px";  // 上下1px, 左右6pxのパディング
+  button.style.marginTop = "8px";    // 上に8pxのマージンを追加
+  button.style.marginBottom = "16px"; // 下に16pxのマージンを追加
+
+  button.style.border = "none";         // ボーダーを消す
+  button.style.borderRadius = "100px";  // 角を丸める（100pxの半径）
+  button.style.backgroundColor = "#2ab6e9";  // 背景色を#2ab6e9に
+
+  button.style.textAlign = "center";  // 文字を中央に配置
+  button.style.color = "white";       // 文字色を白に
+  button.style.fontSize = "14px";     // フォントサイズを14pxに設定
+
+  button.style.cursor = "pointer";    // マウスオーバー時のカーソルをポインタに
 
   // マウスオーバー時の効果を追加
   button.onmouseover = function () {
@@ -38,14 +42,16 @@ function addCalilButton(isbn) {
     button.style.backgroundColor = "#2ab6e9"; // 元の色に戻す
   };
 
-  // ボタンを<div id="addToCart_feature_div">の上に挿入
-  let addToCartElement = document.getElementById("addToCart_feature_div");
-  if (addToCartElement) {
-    addToCartElement.insertAdjacentElement("beforebegin", button);
+  // ボタンを<div id="quantityRelocate_feature_div">の下に挿入
+  let quantityRelocateElement = document.getElementById("quantityRelocate_feature_div");
+  if (quantityRelocateElement) {
+    quantityRelocateElement.insertAdjacentElement("afterend", button);
   }
 
   // ボタンをクリックするとカーリルのページを新しいタブで開く
-  button.onclick = function () {
+  button.onclick = function (event) {
+    event.preventDefault(); // デフォルトの動作を防ぐ
+    event.stopPropagation(); // イベントのバブリングを防ぐ
     window.open(`https://calil.jp/book/${isbn}`, "_blank");
   };
 }
